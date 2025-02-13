429e93f0a45df84788a4deaaa33b0c6cf7d97376
CRCunha <tsi.cristian@outlook.com>
2025-02-13 14:56:04 -0300

    home page content

---
+++ src/pages/home/home.styled.jsx
@@ -0,0 +1,136 @@
+import styled from 'styled-components';
+import {
+  iconContainerColor,
+  primaryText,
+  primary,
+  secondaryText,
+  iconColor,
+} from '../../constants/colors.jsx';
+
+export const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #ffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HomeContent = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  height: calc(100% - 120px);
+
+  display: flex;
+`;
+
+export const ContentContainerLeftSide = styled.div`
+  width: 60%;
+  height: 100%;
+
+  margin-left: 30px;
+`;
+
+export const CarHeader = styled.div`
+  width: 100%;
+  height: 250px;
+
+  border-radius: 15px;
+
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  display: flex;
+`;
+
+export const CarHeaderLeftSide = styled.div`
+  width: 45%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  width: 80%;
+  height: 80px;
+
+  display: flex;
+  align-items: flex-end;
+  font-size: 20px;
+  color: ${primaryText};
+`;
+
+export const CarHeaderLeftSideContent = styled.div`
+  width: 80%;
+  height: calc(100% - 80px);
+
+  display: flex;
+`;
+
+export const CarHeaderLeftSideContentIconCard = styled.div`
+  width: 30%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SquareICon = styled.div`
+  width: 65px;
+  height: 65px;
+
+  border-radius: 15px;
+
+  background-color: ${iconContainerColor};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CarHeaderLeftSideContentText = styled.div`
+  width: 70%;
+  height: 100%;
+`;
+
+export const CarHeaderRightSide = styled.div`
+  width: 55%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const Ilustration = styled.img`
+  height: 108%;
+`;
+
+export const CarHeaderLeftSideContentTextTop = styled.div`
+  width: 100%;
+  height: 50%;
+
+  color: ${primaryText};
+
+  display: flex;
+  align-items: flex-end;
+`;
+
+export const CarHeaderLeftSideContentTextBotton = styled.div`
+  width: 100%;
+  height: 50%;
+
+  display: flex;
+  align-items: flex-start;
+
+  color: ${primary};
+  font-size: 14px;
+
+  cursor: pointer;
+`;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,26 @@
 import { useState } from 'react';
 
-import { MainContainer, HomeContent, ContentContainer } from './default.styled';
+import {
+  MainContainer,
+  HomeContent,
+  ContentContainer,
+  ContentContainerLeftSide,
+  CarHeader,
+  CarHeaderLeftSide,
+  CarHeaderRightSide,
+  Ilustration,
+  Title,
+  CarHeaderLeftSideContent,
+  CarHeaderLeftSideContentIconCard,
+  CarHeaderLeftSideContentText,
+  SquareICon,
+  CarHeaderLeftSideContentTextTop,
+  CarHeaderLeftSideContentTextBotton,
+} from './home.styled';
+
+import { iconColor } from '../../constants/colors.jsx';
+
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
 
 import HeaderBar from '../../components/headerBar';
 
@@ -15,7 +35,38 @@ const Home = (props) => {
     <MainContainer>
       <HomeContent>
         <HeaderBar title="Start" />
-        <ContentContainer></ContentContainer>
+        <ContentContainer>
+          <ContentContainerLeftSide>
+            <CarHeader>
+              <CarHeaderLeftSide>
+                <Title>Bora resolver as pendências?</Title>
+                <CarHeaderLeftSideContent>
+                  <CarHeaderLeftSideContentIconCard>
+                    <SquareICon>
+                      <VideoLibraryOutlinedIcon
+                        style={{ color: iconColor, fontSize: '30px' }}
+                      />
+                    </SquareICon>
+                  </CarHeaderLeftSideContentIconCard>
+                  <CarHeaderLeftSideContentText>
+                    <CarHeaderLeftSideContentTextTop>
+                      Ultima Tarefa
+                    </CarHeaderLeftSideContentTextTop>
+                    <CarHeaderLeftSideContentTextBotton>
+                      Clique para acessar Tarefa
+                    </CarHeaderLeftSideContentTextBotton>
+                  </CarHeaderLeftSideContentText>
+                </CarHeaderLeftSideContent>
+              </CarHeaderLeftSide>
+              <CarHeaderRightSide>
+                <Ilustration
+                  src="https://i.imgur.com/dG9D2Qa.png"
+                  alt="ilustration"
+                />
+              </CarHeaderRightSide>
+            </CarHeader>
+          </ContentContainerLeftSide>
+        </ContentContainer>
       </HomeContent>
     </MainContainer>
   );
